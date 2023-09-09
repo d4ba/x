0a8356f8dae240b7669a34f2386b36792f5bf4eb
dan <117244954+d4ba@users.noreply.github.com>
2023-09-09 18:34:54 -0400

    Fixed error locating

--- FakeMM2RoleMaker.lua
+++ FakeMM2RoleMaker.lua
@@ -4,19 +4,28 @@ local uis = game:GetService("UserInputService")
 
 function creator.createRole(options)
     local success,err = pcall(function()
+        game.Players.LocalPlayer.PlayerGui.MainGUI.Game.RoleSelector.Role.Text = options.RoleName
+        game.Players.LocalPlayer.PlayerGui.MainGUI.Game.RoleSelector.Role.TextColor3 = options.Color
+        game.Players.LocalPlayer.PlayerGui.MainGUI.Game.RoleSelector.Chance.Text = options.ChanceText
+    end)
+    print(success)
+    if success then
         uis.InputBegan:Connect(function(input, gpe)
             if gpe then return end
-            if input.KeyCode == options.KeyCode then
-                game.Players.LocalPlayer.PlayerGui.MainGUI.Game.RoleSelector.Role.Text = options.RoleName
-                game.Players.LocalPlayer.PlayerGui.MainGUI.Game.RoleSelector.Role.TextColor3 = options.Color
-                game.Players.LocalPlayer.PlayerGui.MainGUI.Game.RoleSelector.Chance.Text = options.ChanceText
+            if options.KeyCode == nil then
+                return
+            else
+                if input.KeyCode == options.KeyCode then
+                    game.Players.LocalPlayer.PlayerGui.MainGUI.Game.RoleSelector.Role.Text = options.RoleName
+                    game.Players.LocalPlayer.PlayerGui.MainGUI.Game.RoleSelector.Role.TextColor3 = options.Color
+                    game.Players.LocalPlayer.PlayerGui.MainGUI.Game.RoleSelector.Chance.Text = options.ChanceText
+                end
             end
         end)
-    end)
-    if not success then
-        error("Fake MM2 Role Maker: Couldn't make new fake role!")
-    else
         print("Fake MM2 Role Maker: Successfully created fake role: "..options.RoleName.."!")
+    else
+        warn("Fake MM2 Role Maker: Couldn't make new fake role!")
+        warn("Fake MM2 Role Maker Error: "..err)
     end
 end
 
