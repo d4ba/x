1ff8e917668579b3f68350db0d551eb20d287472
dan <117244954+d4ba@users.noreply.github.com>
2023-09-09 18:02:45 -0400

    Create template.lua

---
+++ FakeMM2RolesMaker/template.lua
@@ -0,0 +1,8 @@
+local rolemaker = loadstring(game:HttpGet('https://raw.githubusercontent.com/d4ba/x/main/FakeMM2RoleMaker.lua'))()
+
+rolemaker.createRole({
+    RoleName = "Role Maker",
+    Color = Color3.fromRGB(124, 229, 255),
+    ChanceText = "Your chance to be Murderer: 69%",
+    KeyCode = Enum.KeyCode.M
+})
